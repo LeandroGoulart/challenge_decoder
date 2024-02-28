5dddf04f4c8fb6663ab2d310be7b26cbc2d39821
LGoulart <leandrovgoulart@gmail.com>
2024-02-28 19:18:27 -0300

    base

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const input_text = document.querySelector
+
+let matriz_comandos = [
+    ["a", "ai"],
+    ["e", "enter"],
+    ["i", "imes"],
+    ["o", "ober"],
+    ["u", "ufat"]
+]
+
+function criptografar (string){
+    
+
+}
+
+
+function descriptografar (){
+
+
+}
